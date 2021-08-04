e50e4ecb90daa39e86d3550974c3b521416b2827
yishayahu <48978288+yishayahu@users.noreply.github.com>
2021-08-04 18:11:31 +0300

    Update cifar.py

--- cifar.py
+++ cifar.py
@@ -255,8 +255,8 @@ def train(trainloader, model, criterion, optimizer, epoch, use_cuda):
         # measure accuracy and record loss
         prec1, prec5 = accuracy(outputs.data, targets.data, topk=(1, 5))
         losses.update(loss.item(), inputs.size(0))
-        top1.update(prec1[0], inputs.size(0))
-        top5.update(prec5[0], inputs.size(0))
+        top1.update(prec1.item(), inputs.size(0))
+        top5.update(prec5.item(), inputs.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
